04f6e2bfca5a11ef307f420987fdd774d676e1cf
Michael <28543330+Schello805@users.noreply.github.com>
2024-12-01 15:40:45 +0100

    Initial Commit

---
+++ ICS-Generator/ICS_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS_GeneratorApp.swift
+//  ICS-Generator
+//
+//  Created by Michael Schellenberger on 01.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ICS_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
